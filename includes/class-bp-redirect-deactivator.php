eee56b5edf8d4a5d3bb771b1ffc6c0e4da5daf66
Wbcom Dev <pallavi@wbcomdesigns.com>
2017-08-16 17:13:02 +0530

    Changed plugin structure according to demo-plugin

---
+++ includes/class-bp-redirect-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://wbcomdesigns.com/contact/
+ * @since      1.0.0
+ *
+ * @package    BP_Redirect
+ * @subpackage BP_Redirect/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    BP_Redirect
+ * @subpackage BP_redirect/includes
+ * @author     Wbcom Designs <admin@wbcomdesigns.com>
+ */
+class BP_Redirect_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
