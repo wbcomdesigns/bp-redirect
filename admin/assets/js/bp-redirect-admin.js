910fcc51b7171daa08a269481b74b24168c3f7d5
Nitin Patil <pallavi@wbcomdesigns.com>
2021-10-19 16:26:59 +0530

    (#28) Update FAQs UI

--- admin/assets/js/bp-redirect-admin.js
+++ admin/assets/js/bp-redirect-admin.js
@@ -129,3 +129,31 @@ jQuery(document).ready(function () {
     }
   });
 });
+
+(function( $ ) {
+	'use strict';
+
+	// Support tab
+	$( document ).ready( function(){
+		var acc = document.getElementsByClassName("bpr-accordion");
+		var i;
+		for (i = 0; i < acc.length; i++) {
+			acc[i].onclick = function () {
+				this.classList.toggle("active");
+				var panel = this.nextElementSibling;
+				if (panel.style.maxHeight) {
+					panel.style.maxHeight = null;
+				} else {
+					panel.style.maxHeight = panel.scrollHeight + "px";
+				}
+			}
+		}
+		$(document).on(
+			'click', '.bpr-accordion', function () {
+				return false;
+			}
+		);
+
+	});
+
+})( jQuery );
