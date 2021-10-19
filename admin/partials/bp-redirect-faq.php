827d6ef5abe23545ab0a88567b7556624e8bf6a8
Wbcom Dev <pallavi@wbcomdesigns.com>
2021-10-19 16:26:59 +0530

    (#28) Update FAQs UI

--- admin/partials/bp-redirect-faq.php
+++ admin/partials/bp-redirect-faq.php
@@ -12,52 +12,52 @@
 <div class="wbcom-tab-content" id="bpr-faq-section">
 		<div class="bp-redirect-tab-header"><h3><?php esc_html_e( 'FAQ(s)', 'bp-redirect' ); ?></h3></div>
 		
-	<div class="bpr-row">
-		<div class="bpr-ques">
+	<div class="bpr-accordion-row">
+		<button class="bpr-accordion">
 			<?php esc_html_e( 'Is this plugin requires another plugin?', 'bp-redirect' ); ?>
-		</div>
-		<div class="bpr-ans">
-			<?php esc_html_e( 'Yes, this plugin requires BuddyPress plugin.', 'bp-redirect' ); ?>
+		</button>
+		<div class="bpr-panel">
+			<p><?php esc_html_e( 'Yes, this plugin requires BuddyPress plugin.', 'bp-redirect' ); ?></p>
 		</div>
 	</div>
-	<div class="bpr-row">
-		<div class="bpr-ques">
+	<div class="bpr-accordion-row">
+		<button class="bpr-accordion">
 			<?php esc_html_e( 'Where it redirects if no option selected or in the case of empty custom URL field?', 'bp-redirect' ); ?>
-		</div>
-		<div class="bpr-ans">
-			<?php esc_html_e( 'In that case, plugin follows default redirection rule.', 'bp-redirect' ); ?>
+		</button>
+		<div class="bpr-panel">
+			<p><?php esc_html_e( 'In that case, plugin follows default redirection rule.', 'bp-redirect' ); ?></p>
 		</div>
 	</div>
-	<div class="bpr-row">
-		<div class="bpr-ques">
+	<div class="bpr-accordion-row">
+		<button class="bpr-accordion">
 			<?php esc_html_e( 'Where it redirects, when we select "Member profile" in BuddyPress Component dropdown?', 'bp-redirect' ); ?>
-		</div>
-		<div class="bpr-ans">
-			<?php esc_html_e( "It redirects to logged in member's profile page", 'bp-redirect' ); ?>
+		</button>
+		<div class="bpr-panel">
+			<p><?php esc_html_e( "It redirects to logged in member's profile page", 'bp-redirect' ); ?></p>
 		</div>
 	</div>
-	<div class="bpr-row">
-		<div class="bpr-ques">
+	<div class="bpr-accordion-row">
+		<button class="bpr-accordion">
 			<?php esc_html_e( 'Where it redirects, when we select "Member activity" in BuddyPress Component dropdown?', 'bp-redirect' ); ?>
-		</div>
-		<div class="bpr-ans">
-			<?php esc_html_e( "It redirects to logged in member's activity page", 'bp-redirect' ); ?>
+		</button>
+		<div class="bpr-panel">
+			<p><?php esc_html_e( "It redirects to logged in member's activity page", 'bp-redirect' ); ?></p>
 		</div>
 	</div>
-	<div class="bpr-row">
-		<div class="bpr-ques">
+	<div class="bpr-accordion-row">
+		<button class="bpr-accordion">
 			<?php esc_html_e( 'Where it redirects, when we select "Activity" in BuddyPress Component dropdown?', 'bp-redirect' ); ?>
-		</div>
-		<div class="bpr-ans">
-			<?php esc_html_e( 'It redirects to logged in site wide activity page', 'bp-redirect' ); ?>
+		</button>
+		<div class="bpr-panel">
+			<p><?php esc_html_e( 'It redirects to logged in site wide activity page', 'bp-redirect' ); ?></p>
 		</div>
 	</div>
-	<div class="bpr-row">
-		<div class="bpr-ques">
+	<div class="bpr-accordion-row">
+		<button class="bpr-accordion">
 			<?php esc_html_e( 'Where do I ask for support?', 'bp-redirect' ); ?>
-		</div>
-		<div class="bpr-ans">
-			<?php esc_html_e( 'Please visit', 'bp-redirect' ); ?> <a href="<?php echo esc_url( 'https://wbcomdesigns.com/contact/' ); ?>" title="<?php esc_attr( 'Wbcom Designs' ); ?>" target="_blank" ><?php esc_html_e( 'Wbcom Designs', 'bp-redirect' ); ?></a> <?php esc_html_e( 'for any query related to plugin and BuddyPress.', 'bp-redirect' ); ?>
+		</button>
+		<div class="bpr-panel">
+			<p><?php esc_html_e( 'Please visit', 'bp-redirect' ); ?> <a href="<?php echo esc_url( 'https://wbcomdesigns.com/contact/' ); ?>" title="<?php esc_attr( 'Wbcom Designs' ); ?>" target="_blank" ><?php esc_html_e( 'Wbcom Designs', 'bp-redirect' ); ?></a> <?php esc_html_e( 'for any query related to plugin and BuddyPress.', 'bp-redirect' ); ?></p>
 		</div>
 	</div>
 </div>
