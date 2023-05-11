658341a5efe99e62cfa4c452b737b9d4e569ef95
Aman kumar <pallavi@wbcomdesigns.com>
2023-05-11 20:14:46 +0530

    (#46) fir redirect uksort error

--- admin/partials/bp-redirect-global-page.php
+++ admin/partials/bp-redirect-global-page.php
@@ -38,14 +38,14 @@ if ( ! empty( $saved_setting ) ) {
 				$val_arr     = $val;
 				$seq[ $key ] = $val_arr;
 		}
-		if ( ! empty( $seq ) ) {
-			uksort(
-				$loginSequence,
-				function ( $key1, $key2 ) use ( $seq ) {
-					return ( array_search( $key1, $seq ) > array_search( $key2, $seq ) );
-				}
-			);
-		}
+		// if ( ! empty( $seq ) ) {
+		// 	uksort(
+		// 		$loginSequence,
+		// 		function ( $key1, $key2 ) use ( $seq ) {
+		// 			return ( array_search( $key1, $seq ) > array_search( $key2, $seq ) );
+		// 		}
+		// 	);
+		// }
 	}
 }
 
@@ -58,14 +58,14 @@ if ( ! empty( $saved_setting ) ) {
 				$logoutseq[ $key ] = $val_arr;
 			
 		}
-		if ( ! empty( $logoutseq ) ) {
-			uksort(
-				$logoutSequence,
-				function ( $logoutkey1, $logoutkey2 ) use ( $logoutseq ) {
-					return ( array_search( $logoutkey1, $logoutseq ) > array_search( $logoutkey2, $logoutseq ) );
-				}
-			);
-		}
+		// if ( ! empty( $logoutseq ) ) {
+		// 	uksort(
+		// 		$logoutSequence,
+		// 		function ( $logoutkey1, $logoutkey2 ) use ( $logoutseq ) {
+		// 			return ( array_search( $logoutkey1, $logoutseq ) > array_search( $logoutkey2, $logoutseq ) );
+		// 		}
+		// 	);
+		// }
 	}
 }
 
