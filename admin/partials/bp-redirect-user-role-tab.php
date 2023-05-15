b95a9bda532602bda2323a5f6dbf784179493017
Wbcom Dev <pallavi@wbcomdesigns.com>
2023-05-15 17:05:13 +0530

    (#55) PHPCS fixes

--- admin/partials/bp-redirect-user-role-tab.php
+++ admin/partials/bp-redirect-user-role-tab.php
@@ -17,15 +17,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <?php
-		$spinner_src   = includes_url() . 'images/spinner.gif';
-		if(class_exists( 'Buddypress' )){
-			$saved_setting = bp_get_option( 'bp_redirect_admin_settings' );
-			$bp_pages      = bp_get_option( 'bp-pages' );
-		}else{
-			$saved_setting = get_option( 'bp_redirect_admin_settings_global' );
-			$bp_pages      = get_pages();
-		}
-		
+		$spinner_src = includes_url() . 'images/spinner.gif';
+if ( class_exists( 'Buddypress' ) ) {
+	$saved_setting = bp_get_option( 'bp_redirect_admin_settings' );
+	$bp_pages      = bp_get_option( 'bp-pages' );
+} else {
+	$saved_setting = get_option( 'bp_redirect_admin_settings_global' );
+	$bp_pages      = get_pages();
+}
+
 		$bp_pages_ids  = array_values( $bp_pages );
 		$loginSequence = $this->get_editable_roles();
 
@@ -33,7 +33,7 @@ if ( ! empty( $saved_setting ) ) {
 	if ( array_key_exists( 'loginSequence', $saved_setting ) ) {
 		$seq = explode( ',', $saved_setting['loginSequence'] );
 		foreach ( $seq as $key => $val ) {
-			
+
 				$val_arr     = $val;
 				$seq[ $key ] = $val_arr;
 		}
@@ -53,7 +53,7 @@ if ( ! empty( $saved_setting ) ) {
 	if ( array_key_exists( 'logoutSequence', $saved_setting ) ) {
 		$logoutseq = explode( ',', $saved_setting['logoutSequence'] );
 		foreach ( $logoutseq as $key => $val ) {
-				$val_arr     = $val;
+				$val_arr           = $val;
 				$logoutseq[ $key ] = $val_arr;
 		}
 		// if ( ! empty( $logoutseq ) ) {
