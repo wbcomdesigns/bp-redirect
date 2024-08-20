13ad08fab82ec93dad8544228503d7c28deae9e8
Varun Kumar Dubey <vapvarun@gmail.com>
2024-08-20 15:16:59 +0530

    removed hard-g

--- gruntfile.js
+++ gruntfile.js
@@ -1,72 +1,70 @@
 'use strict';
+module.exports = function ( grunt ) {
 
-module.exports = function (grunt) {
+	// load all grunt tasks matching the `grunt-*` pattern
+	// Ref. https://npmjs.org/package/load-grunt-tasks
+	require( 'load-grunt-tasks' )( grunt );
+	grunt.initConfig(
+		{
 
-	// Load all grunt tasks matching the `grunt-*` pattern
-	// Reference: https://npmjs.org/package/load-grunt-tasks
-	require('load-grunt-tasks')(grunt);
-
-	// Initialize configuration
-	grunt.initConfig({
-
-		// Check text domain
-		checktextdomain: {
-			options: {
-				text_domain: ['bp-redirect'], // Specify allowed text domain(s)
-				keywords: [ // List of gettext function specifications
-					'__:1,2d',
-					'_e:1,2d',
-					'_x:1,2c,3d',
-					'esc_html__:1,2d',
-					'esc_html_e:1,2d',
-					'esc_html_x:1,2c,3d',
-					'esc_attr__:1,2d',
-					'esc_attr_e:1,2d',
-					'esc_attr_x:1,2c,3d',
-					'_ex:1,2c,3d',
-					'_n:1,2,4d',
-					'_nx:1,2,4c,5d',
-					'_n_noop:1,2,3d',
-					'_nx_noop:1,2,3c,4d'
-				]
-			},
-			target: {
-				files: [{
-					expand: true,
-					src: [
+				// Check text domain
+			checktextdomain: {
+				options: {
+					text_domain: [ 'bp-redirect' ], // Specify allowed domain(s)
+					keywords: [ // List keyword specifications
+						'__:1,2d',
+						'_e:1,2d',
+						'_x:1,2c,3d',
+						'esc_html__:1,2d',
+						'esc_html_e:1,2d',
+						'esc_html_x:1,2c,3d',
+						'esc_attr__:1,2d',
+						'esc_attr_e:1,2d',
+						'esc_attr_x:1,2c,3d',
+						'_ex:1,2c,3d',
+						'_n:1,2,4d',
+						'_nx:1,2,4c,5d',
+						'_n_noop:1,2,3d',
+						'_nx_noop:1,2,3c,4d'
+					]
+				},
+				target: {
+					files: [ {
+						src: [
 						'*.php',
 						'**/*.php',
 						'!node_modules/**',
 						'!options/framework/**',
 						'!tests/**'
-					]
-				}]
-			}
-		},
-
-		// Generate POT files
-		makepot: {
-			target: {
-				options: {
-					cwd: '.', // Directory of files to internationalize
-					domainPath: 'languages/', // Where to save the POT file
-					exclude: ['node_modules/*', 'options/framework/*'], // Files or directories to ignore
-					mainFile: 'index.php', // Main project file
-					potFilename: 'bp-redirect.pot', // Name of the POT file
-					potHeaders: { // Headers to add to the generated POT file
-						poedit: true, // Include common Poedit headers
-						'Last-Translator': 'Varun Dubey',
-						'Language-Team': 'Wbcom Designs',
-						'report-msgid-bugs-to': '',
-						'x-poedit-keywordslist': true // Include a list of all possible gettext functions
-					},
-					type: 'wp-plugin', // Type of project (wp-plugin or wp-theme)
-					updateTimestamp: true // Whether to update the POT-Creation-Date even without other changes
+							], // all php
+						expand: true
+					} ]
+				}
+			},
+				// make po files
+			makepot: {
+				target: {
+					options: {
+						cwd: '.', // Directory of files to internationalize.
+						domainPath: 'languages/', // Where to save the POT file.
+						exclude: [ 'node_modules/*', 'options/framework/*' ], // List of files or directories to ignore.
+						mainFile: 'index.php', // Main project file.
+						potFilename: 'bp-redirect.pot', // Name of the POT file.
+						potHeaders: { // Headers to add to the generated POT file.
+							poedit: true, // Includes common Poedit headers.
+							'Last-Translator': 'Varun Dubey',
+							'Language-Team': 'Wbcom Designs',
+							'report-msgid-bugs-to': '',
+							'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
+						},
+						type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
+						updateTimestamp: true // Whether the POT-Creation-Date should be updated without other changes.
+					}
 				}
 			}
 		}
-	});
+	);
 
-	// Register default tasks
-	grunt.registerTask('default', ['checktextdomain', 'makepot']);
+	// register task  'checktextdomain', 'makepot',
+	grunt.registerTask( 'default', [ 'checktextdomain','makepot' ] );
 };
